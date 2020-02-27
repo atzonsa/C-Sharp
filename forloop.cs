10f4658e08430fee1ab50ab1a291fc5565ff5745
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-02-28 02:06:12 +0530

    For Loop

---
+++ forloop.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Value of i: {0}", i);
+        }
+    }
+}
