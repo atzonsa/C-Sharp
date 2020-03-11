6548e8c536bfa29183febabe81fee28d19be1c75
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-11 13:25:05 +0530

    exception handling

---
+++ ExceptionHandling.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandboxConsole
+{
+    class ExceptionHandling
+    {
+        public static void Main(string[] arg)
+        {
+            int val1 = 33;
+            int val2 = 0;
+
+            try
+            {
+                Console.WriteLine(val1 / val2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.Write("Press an key to close");
+                Console.Read();
+            }
+        }
+    }
+}
