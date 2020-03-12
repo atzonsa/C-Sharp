fa7d56e874ecece03605f808459e1e68b87f2754
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-12 12:40:57 +0530

    switch case

---
+++ SwitchCase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandboxConsole
+{
+    class SwitchCase
+    {
+        static void Main(string[] arg)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter value from below options");
+                Console.WriteLine("10, 20, 30, 40, 50");
+                
+                int val = 0;
+                Int32.TryParse(Console.ReadLine(), out val);
+
+                switch (val)
+                {
+                    case 10:
+                        Console.WriteLine("Ten");
+                        break;
+                    case 20:
+                        Console.WriteLine("Twenty");
+                        break;
+                    case 30:
+                        Console.WriteLine("Thirty");
+                        break;
+                    case 40:
+                        Console.WriteLine("Fourty");
+                        break;
+                    case 50:
+                        Console.WriteLine("Fifty");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid value");
+                        break;
+                }
+            }
+        }
+    }
+}
