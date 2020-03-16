ea6d16858a8651b7c355bef723dbd39d8144b0cb
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-16 14:38:39 +0530

    object reference

---
+++ ObjectReference.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandboxConsole
+{
+    class ObjectReference
+    {
+        public static void Main(string[] arg)
+        {
+            string str = "ssssssssss";
+            char c = Char.Parse("s");
+            String str1 = new String(c, 10);
+            String str2 = new String(c, 10);
+
+            Console.WriteLine("str: " + str);
+            Console.WriteLine("str1: " + str1);
+            Console.WriteLine("str2: " + str2);
+
+            Console.WriteLine((str == str1) ? "equal" : "not equal");
+            Console.WriteLine((str1 == str2) ? "equal" : "not equal");
+            Console.WriteLine((str2 == str) ? "equal" : "not equal");
+
+            Console.WriteLine("Press any key to close");
+            Console.Read();
+        }
+    }
+}
